969044ca0008c5044a604d649055a9a76537bce7
Gabriele Grosse <ggross7577@gmail.com>
2023-05-21 12:35:48 -0400

    new lab 2

---
+++ Lab2/Lab2.java
@@ -0,0 +1,26 @@
+package Lab2;
+
+public class Lab2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private class GenericTest<T> {
+        private String name;
+        private T value;
+
+        public GenericTest(String name, T value){
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return String.format("name: value", name.toString(), value.toString());
+        }
+    }
+
+
+
+}
